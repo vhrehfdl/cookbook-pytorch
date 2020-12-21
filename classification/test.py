c9baf1618002ef7fe00079c4074aeb3b458667b3
LeeJungHoon <vhrehfdl7tp@naver.com>
2020-12-21 19:31:05 +0900

    ELMo Classification 수정
    우선 중요한 부분 모듈화는 했지만 내가 원하는대로 완벽하게 바꾸지는 못했다.
    이는 내가 Python에 대한 지식이 부족해서 그런것으로 생각된다.
    파이썬 자체에 대한 지식도 높여야겠다.

--- classification/test.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn.functional as F
-
-input = torch.randn(3, 5, requires_grad=True)
-target = torch.randint(5, (3,), dtype=torch.int64)
-print(input)
-print(target)
-loss = F.cross_entropy(input, target)
-loss.backward()
